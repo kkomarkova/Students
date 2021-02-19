0dab72a35b2eb242ea929ce305794ea950364607
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-19 14:56:24 +0100

    Adding recyclerView, Intent to AnotherActivity

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -3,28 +3,40 @@ package com.example.students;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    CustomAdapter adapter;
+    private final Student[] allStudents = Students.getAllStudents();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // get the reference of RecyclerView
+        // Want to update the Adapters data when coming back from the child activity
         RecyclerView recyclerView = findViewById(R.id.rvStudents);
-
-        // set a LinearLayoutManager with default vertical orientation
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        //  call the constructor of CustomAdapter to send the reference and data to Adapter
-        CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, personNames);
-        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new CustomAdapter(this,Arrays.asList(allStudents));
+        recyclerView.setAdapter(adapter);
 
     }
 
-
+    public void getdetailsstudent(View view, int position) {
+        Student student = adapter.getItem(position);
+        Intent intent = new Intent(this, AnotherActivity.class);
+        intent.putExtra(AnotherActivity.STUDENT, student);
+        startActivity(intent);
+    }
 }
