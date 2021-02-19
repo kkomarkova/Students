70e9a572de67f31eae33998f90b4eb00b37b8a96
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-19 14:56:45 +0100

    Change name OneStudent

---
+++ app/src/main/java/com/example/students/OneStudent.java
@@ -0,0 +1,31 @@
+package com.example.students;
+
+import java.util.ArrayList;
+
+public class OneStudent {
+    public static void main(String args[])
+    {
+        //Creating objects of students
+        Student student1 = new Student("James", "Bond", 18, "Czech");
+        Student student2 = new Student("John", "Black", 17, "Polish");
+        Student student3 = new Student("Jordan", "White", 18, "British");
+        Student student4 = new Student("Lars", "Peterson", 18, "Danish");
+
+        //Creating new Arraylist of Students
+        ArrayList<Student> arraystudent = new ArrayList<Student>();
+
+        //Adding values
+        arraystudent.add(student1);
+        arraystudent.add(student2);
+        arraystudent.add(student3);
+        arraystudent.add(student4);
+
+        //Printing out the values
+        for(Student student: arraystudent){
+
+            System.out.println(student);
+        }
+    }
+
+}
+
