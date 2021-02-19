34e701fac5a9f7eb64130a7df7742ae5ef138015
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-19 13:55:12 +0100

    Get the reference of RecyclerView

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.students;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the reference of RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvStudents);
+
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+        CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, personNames);
+        recyclerView.setAdapter(customAdapter);
+
     }
+
+
 }
