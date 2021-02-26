1a6e85322a443b6181261a945342b338dbf386ed
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-26 11:00:51 +0100

    setClickListener
    Adding method on adapter from my CustomAdapter to be able to get the detail of student on the next page

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -18,12 +20,14 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     CustomAdapter adapter;
+    private static final String LOG_TAG = "STUDENTS";
     private final Student[] allStudents = Students.getAllStudents();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Orange", allStudents.toString());
 
         // Want to update the Adapters data when coming back from the child activity
         RecyclerView recyclerView = findViewById(R.id.rvStudents);
@@ -31,7 +35,19 @@ public class MainActivity extends AppCompatActivity {
         adapter = new CustomAdapter(this,Arrays.asList(allStudents));
         recyclerView.setAdapter(adapter);
 
+        adapter.setClickListener(new CustomAdapter.ItemClickListener() {
+
+            //@Override
+            public void onItemClick(View view, int position) {
+                Student student = adapter.getItem(position);
+                Log.d(LOG_TAG, "Clicked " + position + " " + adapter.getItemId(position) + " " + student);
+                Intent intent = new Intent(getBaseContext(), AnotherActivity.class);
+                intent.putExtra(AnotherActivity.STUDENT, student );
+                startActivity(intent);
+            }
+        });
     }
+
 
     public void getdetailsstudent(View view, int position) {
         Student student = adapter.getItem(position);
